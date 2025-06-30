7fb6b8f4cb313c294565a3af671e0f93727fabd1
dandelion <1993466159@qq.com>
2025-06-30 23:47:35 +0800

    配置SimpleMDE

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,19 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+"use client";
+
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
-    <div className="space-y-5">
-      <TextField.Root placeholder="请输入问题的标题" className=""></TextField.Root>
-      <TextArea placeholder="请输入问题的描述" />
+    <div className="max-w-xl space-y-5">
+      <TextField.Root
+        placeholder="请输入问题的标题"
+      ></TextField.Root>
+      <SimpleMDE />
       <Button>提交新问题</Button>
     </div>
   );
