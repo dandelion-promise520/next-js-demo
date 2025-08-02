b06d797e0f9f9b43180f6f4b0d6dcfc70f5859f3
dandeion <1993466159@qq.com>
2025-08-02 11:20:10 +0800

    重构 MdEditor 组件，简化 setValue 类型定义；更新新问题页面，使用 react-hook-form 处理表单提交；移除不必要的状态管理；更新 Prisma schema，清理多余空行

--- app/components/MdEditor/MdEditor.tsx
+++ app/components/MdEditor/MdEditor.tsx
@@ -4,17 +4,15 @@ import gfm from "@bytemd/plugin-gfm";
 import gemoji from "@bytemd/plugin-gemoji";
 import highlight from "@bytemd/plugin-highlight";
 import zhCN from "bytemd/locales/zh_Hans.json";
-import React from "react";
 const plugins = [
   gfm(),
   gemoji(),
   highlight(),
   // Add more plugins here
 ];
-
 interface MdEditorProps {
   value: string;
-  setValue: React.Dispatch<React.SetStateAction<string>>;
+  setValue: (v: string) => void;
 }
 
 const MdEditor: React.FC<MdEditorProps> = ({ value, setValue }) => {
@@ -25,9 +23,7 @@ const MdEditor: React.FC<MdEditorProps> = ({ value, setValue }) => {
       locale={zhCN}
       value={value}
       plugins={plugins}
-      onChange={(v) => {
-        setValue(v);
-      }}
+      onChange={setValue}
     />
   );
 };
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,30 +1,53 @@
 "use client";
 
 import { Button, TextField } from "@radix-ui/themes";
-import { useState } from "react";
 import MdEditor from "@/app/components/MdEditor/MdEditor";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
+interface IFormInput {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
-  // 标题状态
-  const [title, setTitle] = useState();
-
-  // 标题更改事件
-  const handleChange = (e:any) => {
-    setTitle(e.target.value);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+  // 表单提交处理函数
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
   };
-
-  // 描述的状态
-  const [description, setDescription] = useState("");
-
   return (
-    <form className="max-w-3xl space-y-5">
-      <TextField.Root
-      value={title}
-        onChange={handleChange}
-        placeholder="请输入问题的标题"
-      ></TextField.Root>
-      <MdEditor value={description} setValue={setDescription} />
+    <form className="max-w-3xl space-y-5" onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="title"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <TextField.Root
+            {...field}
+            placeholder="请输入问题的标题"
+          ></TextField.Root>
+        )}
+      />
+      {errors.title && <p role="alert">{errors.title.message}</p>}
+
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <MdEditor value={field.value} setValue={field.onChange} />
+        )}
+      />
+      {errors.description && <p role="alert">{errors.description.message}</p>}
+
       <Button type="submit">提交新问题</Button>
     </form>
   );
