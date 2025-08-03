a51a00109835a2b463300df6e2ffaeef2edd5fdd
dandeion <1993466159@qq.com>
2025-08-03 09:09:21 +0800

    docker打包配置

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const { codeInspectorPlugin } = require("code-inspector-plugin");
 const nextConfig = {
-    webpack: (config, { dev, isServer }) => {
-        config.plugins.push(codeInspectorPlugin({
-            bundler: "webpack",
-            editor: 'code',
-            showSwitch: true
-        }));
-        return config;
-    },
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(
+      codeInspectorPlugin({
+        bundler: "webpack",
+        editor: "code",
+        showSwitch: true,
+      })
+    );
+    return config;
+  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
